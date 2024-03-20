28b6d3d7f7ffd07ef485d274db58a0a42f5b261c
cppshizoid <alex.krivoschecov@gmail.com>
2024-03-20 15:38:28 +0700

    fixed macos install

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ elif [ -x "$(command -v dnf)" ]; then
     sudo dnf install -y @development-tools neofetch git clang clang-tools-extra gcc cmake ninja-build lld lldb valgrind python3-pip gtest doxygen neovim qt5-qtbase-devel qt5-qtbase qt6-core qt6-qtbase qt6-qtbase-devel qt6-qtmutimedia  glfw glm-devel glew vulkan-headers vulkan-loader vulkan-tools vulkan-volk-devel glslang spirv-tools spirv-llvm-translator
 elif [ -x "$(command -v pacman)" ]; then
     sudo pacman -Syyu -noconfirm base-devel neofetch neovim lua git clang compiler-rt gcc cmake ninja make lld lldb valgrind gtest qt5-base qt5-multimedia qt5-quick3d qt6-tools qt6-quick3d qt6-multimedia glfw glew glm vulkan-extra-layers vulkan-extra-tools vulkan-headers vulkan-tools vulkan-validation-layers spirv-llvm-translator
-elif
+elif [ -x "$(command -v brew)" ]; then
     brew install xcodebuild neofetch neovim python3  git clang cmake ninja make lld lldb valgrind qt5  qt6 glfw glew glm vulkan-headers vulkan-loader vulkan-tools vulkan-extenstionlayer vulkan-validationlayer spirv-cross spirv-headers spirv-llvm-translator
 else
     echo "Не удалось определить дистрибутив и установщик пакетов."
@@ -28,5 +28,3 @@ cd build
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 cmake --build .
 ./test
-
-
