a0934bba143cbb80bb9b6465e04e31eaa09005fb
cppshizoid <alex.krivoschecov@gmail.com>
2024-06-26 09:38:24 +0700

    vim plug add to script

--- run.sh
+++ run.sh
@@ -51,6 +51,9 @@ source $ZSH/oh-my-zsh.sh
 ' > ~/.zshrc
 
 source ~/.zshrc
+# Setup nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 mkdir ~/.config/nvim
 
