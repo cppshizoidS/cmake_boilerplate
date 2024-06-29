6aca107c4f5099f99ae13cab643ae21d658256c0
cppshizoid <alex.krivoschecov@gmail.com>
2024-06-29 08:15:58 +0700

    linting and formatting scripts for ci

---
+++ lint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define source files
+SOURCE_FILES=$(find src include -name '*.cpp' -o -name '*.hpp' -o -name '*.h' -o -name '*.cppm' -o -name '*.ixx')
+
+# Run clang-tidy
+clang-tidy $SOURCE_FILES -- -std=c++23
